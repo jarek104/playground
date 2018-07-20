8d0d2b93eac81a7fa124861f06576c3cc39f71f0
Jerry Olewniczak <jerry.olewniczak@hyland.com>
2018-07-20 14:54:58 -0400

    cleaned up

--- src/app/components/xp-accordion-panel-body/xp-accordion-panel-body.component.ts
+++ src/app/components/xp-accordion-panel-body/xp-accordion-panel-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-xp-accordion-panel-body',
--- src/app/components/xp-accordion-panel/xp-accordion-panel.component.ts
+++ src/app/components/xp-accordion-panel/xp-accordion-panel.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit, Input, HostBinding, OnChanges, SimpleChanges, ContentChildren, QueryList, AfterContentInit, ContentChild } from '@angular/core';
-import { query } from '../../../../node_modules/@angular/animations';
-import { XpAccordionPanelBodyComponent } from '../xp-accordion-panel-body/xp-accordion-panel-body.component';
 
 @Component({
   selector: 'app-xp-accordion-panel',
   templateUrl: './xp-accordion-panel.component.html',
   styleUrls: ['./xp-accordion-panel.component.scss']
 })
-export class XpAccordionPanelComponent implements OnInit, AfterContentInit {
-
-  @ContentChild(XpAccordionPanelBodyComponent) panelBody: XpAccordionPanelBodyComponent;
+export class XpAccordionPanelComponent {
 
   @HostBinding('class.expand')
   @Input() expanded = false;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
-  ngAfterContentInit(): void {
-    // console.log(this.panelBody);
-  }
 
 }
